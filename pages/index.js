32f9cbe005eeb7f5c788786435676cd6311f8881
Eduardo P. Silveira <edupsilveira@gmail.com>
2021-01-25 17:25:24 -0300

    Add meta no Head da página

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components'
-import db from '../db.json'
+import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
-
+import QuizContainer from '../src/components/QuizContainer'
+import Head from 'next/head';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -13,38 +13,36 @@ import GitHubCorner from '../src/components/GitHubCorner'
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
-        <Widget>
-          <Widget.Header>
-            <h1>Quiz Da Bíblia</h1>
-          </Widget.Header>
-          <Widget.Content>            
-            <p>Teste seus conhecimentos bíblicos!</p>            
-          </Widget.Content>
-        </Widget>
-        <Widget>
-          <Widget.Content>            
-            <h1>Quizes Da Galera</h1>
-            <p>Outros quizes do pessoal da Imersão Alura!</p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
-      </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/epsilveira"/>
-    </QuizBackground>
+    <>
+      <Head>
+        <meta property="og:image" content={db.bg} />
+        <title>{db.title}</title>
+        <meta property="og:title" content={db.title} key="title" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {/* <link rel="icon" type="image/png" href={db.icon} /> */}
+      </Head>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>Quiz Da Bíblia</h1>
+            </Widget.Header>
+            <Widget.Content>            
+              <p>Teste seus conhecimentos bíblicos!</p>            
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Content>            
+              <h1>Quizes Da Galera</h1>
+              <p>Outros quizes do pessoal da Imersão Alura!</p>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/epsilveira"/>
+      </QuizBackground>
+    </>
   );
 }
