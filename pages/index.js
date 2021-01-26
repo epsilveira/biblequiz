e9277036266bae8b28bd267308c00f9401035104
Eduardo P. Silveira <edupsilveira@gmail.com>
2021-01-26 10:00:25 -0300

    Aula 2 Finalizada

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizContainer from '../src/components/QuizContainer'
+import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizContainer from '../src/components/QuizContainer';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -14,35 +16,51 @@ import Head from 'next/head';
 // `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
-    <>
+    <QuizBackground backgroundImage={db.bg}>
       <Head>
         <meta property="og:image" content={db.bg} />
         <title>{db.title}</title>
         <meta property="og:title" content={db.title} key="title" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        {/* <link rel="icon" type="image/png" href={db.icon} /> */}
+        <link rel="icon" type="image/png" href={db.icon} />
       </Head>
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
-          <Widget>
-            <Widget.Header>
-              <h1>Quiz Da Bíblia</h1>
-            </Widget.Header>
-            <Widget.Content>            
-              <p>Teste seus conhecimentos bíblicos!</p>            
-            </Widget.Content>
-          </Widget>
-          <Widget>
-            <Widget.Content>            
-              <h1>Quizes Da Galera</h1>
-              <p>Outros quizes do pessoal da Imersão Alura!</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl="https://github.com/epsilveira"/>
-      </QuizBackground>
-    </>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz Da Bíblia</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {/* {name} */}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes Da Galera</h1>
+            <p>Outros quizes do pessoal da Imersão Alura!</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/epsilveira" />
+    </QuizBackground>
   );
 }
